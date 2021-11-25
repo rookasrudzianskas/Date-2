926a0c638b72add77e2309a37558e5f4a14db736
Rokas <rookas.rudzenskas@gmail.com>
2021-11-25 10:23:08 +0200

    Rokas

--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, {createContext} from 'react';
 import {Text, View, StyleSheet} from 'react-native';
+import * as Google from 'expo-google-app-auth';
+
+
+const AuthContext = createContext({
+    // initial state (empty)
+});
 
 export const AuthProvider = ({ children }) => {
     return (
-        <>
+        <AuthContext.Provider value={null}>
             {children}
-        </>
+        </AuthContext.Provider>
     );
 };
 

bd1f72fe88e82a18bebbd45af45ccedb0fa11d48
Rokas <rookas.rudzenskas@gmail.com>
2021-11-25 16:06:33 +0200

    Rokas

--- screens/ModalScreen/ModalScreen.js
+++ screens/ModalScreen/ModalScreen.js
@@ -1,5 +1,5 @@
 import React, {useLayoutEffect} from 'react';
-import {Text, View, StyleSheet, Image} from 'react-native';
+import {Text, View, StyleSheet, Image, TextInput} from 'react-native';
 import {useNavigation} from "@react-navigation/native";
 import tw from "tailwind-rn";
 import useAuth from "../../hooks/useAuth";
@@ -27,6 +27,44 @@ const ModalScreen = () => {
             <Text style={tw('text-xl text-gray-500 p-2 font-bold')}>
                 Welcome {user?.displayName}
             </Text>
+
+
+            {/* the profile pic 📹 ------------- */}
+            <Text style={tw('text-center p-4 font-bold text-red-400')}>
+                Step 1: The Profile Pic 🖼️
+            </Text>
+
+            <TextInput
+                style={tw('text-center text-xl pb-2')}
+                placeholder="Enter Profile Pic URL"
+            />
+
+            {/* end of the profile pic 📹 ------------- */}
+
+            {/* the job 📹 ------------- */}
+            <Text style={tw('text-center p-4 font-bold text-red-400')}>
+                Step 2: The Job 📝
+            </Text>
+
+            <TextInput
+                style={tw('text-center text-xl pb-2')}
+                placeholder="Enter your occupation"
+            />
+
+            {/* end of job 📹 ------------- */}
+
+            {/* the age 📹 ------------- */}
+            <Text style={tw('text-center p-4 font-bold text-red-400')}>
+                Step 3: The Age 👴
+            </Text>
+
+            <TextInput
+                style={tw('text-center text-xl pb-2')}
+                placeholder="Enter your age"
+            />
+
+            {/* end of the age 📹 ------------- */}
+
 
         </View>
     );

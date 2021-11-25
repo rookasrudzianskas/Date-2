2985e63af2dba9aa16891ff540fdb28b5cc5abcd
Rokas <rookas.rudzenskas@gmail.com>
2021-11-25 13:28:48 +0200

    Rokas

--- screens/LoginScreen/LoginScreen.js
+++ screens/LoginScreen/LoginScreen.js
@@ -9,7 +9,9 @@ const LoginScreen = () => {
     const navigation = useNavigation();
 
     useLayoutEffect(() => {
-
+        navigation.setOptions({
+            headerShown: false
+        })
     }, []);
 
     return (

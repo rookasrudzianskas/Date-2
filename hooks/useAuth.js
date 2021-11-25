95e4ebe8f0f7d6abc0ca0731caccdbea5c94a3e2
Rokas <rookas.rudzenskas@gmail.com>
2021-11-25 12:00:19 +0200

    Rokas:

--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -56,7 +56,7 @@ export const AuthProvider = ({ children }) => {
 
     return (
         <AuthContext.Provider value={{
-            user: user,
+            user,
             signInWithGoogle
         }}>
             {children}

465194754a4a1a88dd2cdb252249a865fe89aa67
Rokas <rookas.rudzenskas@gmail.com>
2021-11-25 16:01:49 +0200

    Rokas

--- screens/ModalScreen/ModalScreen.js
+++ screens/ModalScreen/ModalScreen.js
@@ -1,6 +1,8 @@
 import React, {useLayoutEffect} from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet, Image} from 'react-native';
 import {useNavigation} from "@react-navigation/native";
+import tw from "tailwind-rn";
+import useAuth from "../../hooks/useAuth";
 
 const ModalScreen = () => {
 
@@ -12,11 +14,20 @@ const ModalScreen = () => {
         });
     }, []);
 
+    const {user} = useAuth();
+
     return (
-        <View>
-            <Text>
-                byrookas 🚀
+        <View style={tw('flex-1 items-center pt-1')}>
+            <Image
+                style={tw('h-20 w-full')}
+                resizeMode={'contain'}
+                source={{ uri: 'https://links.papareact.com/2pf'}}
+            />
+
+            <Text style={tw('text-xl text-gray-500 p-2 font-bold')}>
+                Welcome {user?.displayName}
             </Text>
+
         </View>
     );
 };

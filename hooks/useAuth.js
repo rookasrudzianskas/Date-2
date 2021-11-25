ca78886d05960f490dc8097d7652e3be698b3403
Rokas <rookas.rudzenskas@gmail.com>
2021-11-25 10:25:51 +0200

    Rokas

--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -1,4 +1,4 @@
-import React, {createContext} from 'react';
+import React, {createContext, useContext} from 'react';
 import {Text, View, StyleSheet} from 'react-native';
 import * as Google from 'expo-google-app-auth';
 
@@ -9,9 +9,14 @@ const AuthContext = createContext({
 
 export const AuthProvider = ({ children }) => {
     return (
-        <AuthContext.Provider value={null}>
+        <AuthContext.Provider value={{
+            user: 'Rokas'
+        }}>
             {children}
         </AuthContext.Provider>
     );
 };
 
+export default function useAuth() {
+    return useContext(AuthContext);
+}
--- screens/LoginScreen/LoginScreen.js
+++ screens/LoginScreen/LoginScreen.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
+import useAuth from "../../hooks/useAuth";
 
 const LoginScreen = () => {
+    const {user} = useAuth();
+
     return (
         <View>
             <Text>Login to the app</Text>
+            <Text>{user}</Text>
         </View>
     );
 };

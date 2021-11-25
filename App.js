2f7c6abb9f91e5012a3fcb97899df9aac5c1ec90
Rokas <rookas.rudzenskas@gmail.com>
2021-11-25 09:58:57 +0200

    Rokas

--- App.js
+++ App.js
@@ -2,14 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import {Button, SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import tw from "tailwind-rn";
+import StackNavigator from "./navigation/StackNavigator";
+import {NavigationContainer} from "@react-navigation/native";
 
 export default function App() {
   return (
-    <View style={tw('flex-1 justify-center items-center')}>
-      <Text style={tw('font-bold')}>Rokas 🔥</Text>
-        <Button title="Click me" />
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+        <StackNavigator />
+        <StatusBar style="auto" />
+    </NavigationContainer>
   );
 }
 
--- navigation/StackNavigator.js
+++ navigation/StackNavigator.js
@@ -2,10 +2,10 @@ import React from 'react';
 import {Text, View, StyleSheet} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from "../screens/HomeScreen";
 
 
 const Stack = createNativeStackNavigator();
-
 
 const StackNavigator = () => {
 

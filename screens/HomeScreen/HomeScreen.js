9605f68731f37debeda0ed4b7989df84cad5aef2
Rokas <rookas.rudzenskas@gmail.com>
2021-11-25 14:27:28 +0200

    Rokas

--- screens/HomeScreen/HomeScreen.js
+++ screens/HomeScreen/HomeScreen.js
@@ -21,16 +21,22 @@ const HomeScreen = () => {
         <SafeAreaView>
             {/* header */}
 
-            <View>
-                <TouchableOpacity activeOpacity={0.5}>
+            <View style={tw('items-center relative')}>
+                <TouchableOpacity activeOpacity={0.5} style={tw('absolute left-5 top-3')}>
                     <Image source={{ uri: user?.photoURL }} style={tw('h-10 w-10 rounded-full')} />
+                </TouchableOpacity>
+
+                <TouchableOpacity activeOpacity={0.5}>
+                    <Image source={require('../../assets/tinder.png')} style={tw('h-14 w-14')} />
                 </TouchableOpacity>
 
             </View>
 
+
+
             {/* End of the header */}
-            <Button onPress={() => navigation.navigate('Chat')} title={'Go to chat Screen'}/>
-            <Button onPress={logout} title={'Log Out'}/>
+            {/*<Button onPress={() => navigation.navigate('Chat')} title={'Go to chat Screen'}/>*/}
+            {/*<Button onPress={logout} title={'Log Out'}/>*/}
         </SafeAreaView>
     );
 };

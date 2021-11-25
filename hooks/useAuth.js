9780db62a5af3202339b80baa1b63fe1fcd7fdce
Rokas <rookas.rudzenskas@gmail.com>
2021-11-25 10:16:36 +0200

    Rokas

---
+++ hooks/useAuth.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+export const AuthProvider = () => {
+    return (
+        <View>
+
+        </View>
+    );
+};
+

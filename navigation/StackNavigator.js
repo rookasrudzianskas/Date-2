e19d76b2c3955488302db04b12d92c8b2cecb527
Rokas <rookas.rudzenskas@gmail.com>
2021-11-25 09:55:24 +0200

    Rokas

--- navigation/StackNavigator.js
+++ navigation/StackNavigator.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import {Text, View, StyleSheet} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+const Stack = createNativeStackNavigator();
+
 
 const StackNavigator = () => {
+
+
+
     return (
-        <View>
-            <Text>
-                byrookas 🚀
-            </Text>
-        </View>
+        <Stack.Navigator>
+            <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
     );
 };
 

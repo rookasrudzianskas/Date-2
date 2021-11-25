930747d57ad3f07ac5b062577b43ffd98db25f8b
Rokas <rookas.rudzenskas@gmail.com>
2021-11-25 11:22:55 +0200

    Rokas

--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -11,6 +11,7 @@ const AuthContext = createContext({
 
 
 const config = {
+    androidClientId: '665993343465-l1tdtfsaongopqarudmb7q38q3icmlb8.apps.googleusercontent.com',
     iosClientId: '665993343465-gv4v6pb0iv071it8jpdbn02a6go2pg2n.apps.googleusercontent.com',
     scopes: ['profile', 'email'],
     permissions: ['public_profile', 'email', 'gender', 'location'],

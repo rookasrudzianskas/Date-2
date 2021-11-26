d17dd52c14a955249991273090bfd67fb2c16d90
Rokas <rookas.rudzenskas@gmail.com>
2021-11-26 10:41:17 +0200

    Rokas

--- components/ChatRow.js
+++ components/ChatRow.js
@@ -17,10 +17,23 @@ const ChatRow = ({matchDetails}) => {
     }, [matchDetails, user]);
 
     return (
-        <TouchableOpacity style={tw('flex-row items-center py-3 px-5 bg-white mx-3 my-1 rounded-lg')}>
+        <TouchableOpacity style={[tw('flex-row items-center py-3 px-5 bg-white mx-3 my-1 rounded-lg'), styles.cardShadow]}>
             <Image style={tw('rounded-full h-16 w-16 mr-4')} source={{ uri: matchedUserInfo?.photoURL}} />
         </TouchableOpacity>
     );
 };
 
 export default ChatRow;
+
+const styles = StyleSheet.create({
+    cardShadow: {
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 1,
+        },
+        shadowOpacity: 0.2,
+        shadowRadius: 1.41,
+        elevation: 2,
+    }
+})

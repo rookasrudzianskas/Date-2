49d57b4eee6869f7179024849dcdd3195c667b46
Rokas <rookas.rudzenskas@gmail.com>
2021-11-26 10:36:16 +0200

    Rokas

--- components/ChatRow.js
+++ components/ChatRow.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Text, View, StyleSheet, TouchableOpacity, Image} from 'react-native';
 import {useNavigation} from "@react-navigation/native";
 import useAuth from "../hooks/useAuth";
+import getMatchedUserInfo from "../lib/getMatchedUserInfo";
 
 const ChatRow = ({matchDetails}) => {
 
     const navigation = useNavigation();
     const {user} = useAuth();
 
+    useEffect(() => {
+        setMatchedUserInfo(getMatchedUserInfo(matchDetails.users, user.uid));
+    }, [matchDetails, user]);
 
     return (
         <TouchableOpacity>

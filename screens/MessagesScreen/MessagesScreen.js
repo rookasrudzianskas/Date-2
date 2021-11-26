f0832ece0383501ba8b709c35787ded57c20ad46
Rokas <rookas.rudzenskas@gmail.com>
2021-11-26 12:20:14 +0200

    ROkas

--- screens/MessagesScreen/MessagesScreen.js
+++ screens/MessagesScreen/MessagesScreen.js
@@ -1,5 +1,5 @@
-import React, {useLayoutEffect} from 'react';
-import {Text, View, StyleSheet, SafeAreaView} from 'react-native';
+import React, {useLayoutEffect, useState} from 'react';
+import {Text, View, StyleSheet, SafeAreaView, TextInput} from 'react-native';
 import Header from "../../components/Header";
 import {useNavigation, useRoute} from "@react-navigation/native";
 import getMatchedUserInfo from "../../lib/getMatchedUserInfo";
@@ -9,6 +9,8 @@ const MessageScreen = () => {
 
     const navigation = useNavigation();
     const {params} = useRoute();
+    // please define the state for the input with blank string
+    const [input, setInput] = useState('');
 
     const { matchDetails } = params;
     const {user} = useAuth();
@@ -19,9 +21,17 @@ const MessageScreen = () => {
         });
     }, []);
 
+    const sendMessage = () => {
+
+    }
+
     return (
         <SafeAreaView>
             <Header callEnabled={true} title={getMatchedUserInfo(matchDetails?.users, user.uid).displayName} />
+
+            <View>
+                <TextInput style={tw('h-10 text-lg')} placeholder="Send a message..." onChangeText={setInput} onSubmitEditing={sendMessage} />
+            </View>
         </SafeAreaView>
     );
 };

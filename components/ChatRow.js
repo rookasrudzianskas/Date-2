61d1ffd7e7208d5b5411915da0a4c9f0914cc4c7
Rokas <rookas.rudzenskas@gmail.com>
2021-11-26 10:39:26 +0200

    Rokas

--- components/ChatRow.js
+++ components/ChatRow.js
@@ -1,21 +1,24 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Text, View, StyleSheet, TouchableOpacity, Image} from 'react-native';
 import {useNavigation} from "@react-navigation/native";
 import useAuth from "../hooks/useAuth";
 import getMatchedUserInfo from "../lib/getMatchedUserInfo";
+import tw from "tailwind-rn";
 
 const ChatRow = ({matchDetails}) => {
 
     const navigation = useNavigation();
     const {user} = useAuth();
+    // define the state for the matchedUserInfo with default value null
+    const [matchedUserInfo, setMatchedUserInfo] = useState(null);
 
     useEffect(() => {
         setMatchedUserInfo(getMatchedUserInfo(matchDetails.users, user.uid));
     }, [matchDetails, user]);
 
     return (
-        <TouchableOpacity>
-            <Image source={{ uri: }} />
+        <TouchableOpacity style={tw('flex-row items-center py-3 px-5 bg-white mx-3 my-1 rounded-lg')}>
+            <Image style={tw('rounded-full h-16 w-16 mr-4')} source={{ uri: matchedUserInfo?.photoURL}} />
         </TouchableOpacity>
     );
 };
